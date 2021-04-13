8b9ea6eb57573c6a726d76dc2f1c2fb50e23b0d2
Zahid Hasan <zhdruvo@gmail.com>
2021-04-14 02:57:07 +0600

    template and static fies added

--- visitent/settings.py
+++ visitent/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'visit_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
