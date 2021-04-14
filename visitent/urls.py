67523bbef0a588b99b61ae283da647dac91bca77
Zahid Hasan <zhdruvo@gmail.com>
2021-04-14 18:50:29 +0600

    blog app added

--- visitent/urls.py
+++ visitent/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include("visit_app.urls"))
+    path('', admin.site.urls),
+    path('visit/', include("visit_app.urls")),
+    path('blog/', include("blog_app.urls"))
 ]
